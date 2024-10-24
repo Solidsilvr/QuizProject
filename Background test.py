69e05f87d5760a798250b2f37859f67824f95982
Solid_silvr <184729496+Solidsilvr@users.noreply.github.com>
2024-10-24 23:19:43 +0530

    Background test.py
    latest model

--- Background test.py
+++ Background test.py
@@ -1,28 +1,53 @@
 import PySimpleGUI as sg
 font = ("Helvetica", 20)
 sg.set_options(font=font)
-sg.theme("PythonPlus")
+sg.theme("LightPurple")
 sg.Window._move_all_windows = True
-sg.theme_background_color("#001d3c")
+sg.theme_background_color("#B0AAC2")
+
+fin_bg_menu=True
+kot_menu=True
+
+timecount=30
+Score=0
+
 background_layout=[[sg.Image(source="Background_image.png")]]
-window_background = sg.Window('Background', background_layout, no_titlebar=True, finalize=True, margins=(0, 0), element_padding=(0,0))
-menu    =[[sg.Push(),sg.Text("Sree Narayana Public School Poothotta"),sg.Push()],
-          [sg.Push(),sg.Text("ComputerScience Department\n"),sg.Push()],
-          [sg.Push(),sg.Text("Take A Quiz\n"),sg.Push()],
-          [sg.Text("           Select A Category")],
-          [sg.Push(),sg.Slider((1,3),orientation="horizontal",size=(12,14))],
-          [sg.Button("MATHS",size=(25,1)),sg.Text("                                                                          Difficulty")],
+window_background = sg.Window('Background', background_layout, no_titlebar=True, finalize=fin_bg_menu, margins=(0, 0), element_padding=(0,0))
+menu    =[[sg.Push(),sg.Text("Sree Narayana Public School Poothotta",font="Verdana 24 italic",pad=(0,(11,0))),sg.Push()],
+          [sg.Push(),sg.Text("ComputerScience Department\n",font="Verdana 24 italic"),sg.Push()],
+          [sg.Push(),sg.Text("Take A Quiz\n",font="Helvetica 21"),sg.Push()],
+          [sg.Text("           Select A Category",font="Helvetica 21")],
+          [sg.Push(),sg.Slider((1,3),orientation="horizontal",size=(12,14),pad=((0,9),0))],
+          [sg.Button("MATHS",size=(25,1)),sg.Text("                                                                         Difficulty",font="Helvetica 22")],
           [sg.Button("COMPUTER SCIENCE",size=(25,1)),sg.Push()],
           [sg.Button("SCIENCE",size=(25,1)),sg.Push(),sg.Text("Follow Me On Github")],
           [sg.Button("ENGLISH",size=(25,1)),sg.Push(),sg.Button("@Solidsilvr",size=(9,1))],
           [sg.Button("GK",size=(25,1)),sg.Push(),sg.Button("QUIT",size=(9,1))]
           ]
-menuwin = sg.Window("Main Menu",menu,size=(1200,620),finalize=True, keep_on_top=True, grab_anywhere=False,  transparent_color=sg.theme_background_color(), no_titlebar=True)
+menuwin = sg.Window("Main Menu",menu,size=(1200,620),finalize=fin_bg_menu, keep_on_top=kot_menu, grab_anywhere=False,  transparent_color=sg.theme_background_color(), no_titlebar=True)
+qz_lo  =[[sg.Push(),sg.Text("Question 1"),sg.Push(),sg.Text(key="Timer",font="Courier 24 bold",text_color="white",size=(6,1),justification='right')],
+          [sg.Push(),sg.Button("A"),sg.Button("B"),sg.Button("C"),sg.Button("D"),sg.Push()]
+          ]
+qz_win = sg.Window("Quiz Window",qz_lo)
 while True:
     window, event, values = sg.read_all_windows()
     print(event, values)
-    if event == "QUIT":
+    if event in ("QUIT","MATHS","SCIENCE","ENGLISH","COMPUTER SCIENCE","GK"):
         print(f'closing window = {window.Title}')
+        menuwin.close()
+        window_background.close()
+        fin_bg_menu=False
+        kot_menu=False
+    if event == "MATHS": 
+        while timecount >= 0:
+            event2 , values2 = qz_win.read(timeout=1000)
+            qz_win["Timer"].update(timecount)
+            timecount=timecount-1
+            if event2 == "A":
+                Score=Score+1
+                break
+    qz_win.close()
+    read="Your Score = "+str(Score)+"/5\nReturn to Main Menu"
+    if sg.popup_yes_no(read,no_titlebar=True,modal=True) == "Yes":
         break
-menuwin.close()
-window_background.close()
+    break
